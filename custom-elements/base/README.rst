7589b83f82f14853de02c44ef4d3b8850b77e52d
sean <sean@seanmooney.info>
2018-04-13 21:40:35 +0100

    inital commit
    - This chage introduces a prep.sh script to bootstrap
      a new build host with all depencies.
    - This change introduces a bsd license file.
    - This change create a base element for future elements.
    - This change declares diskimage-build as a required
      module.

---
+++ custom-elements/base/README.rst
@@ -0,0 +1,14 @@
+====
+BASE
+====
+
+
+Base custom element.
+
+This element dose nothing to the final image.
+it defines a noop job that echo a message in
+each phase of the build.
+
+
+when createing customs elemets can copy this element
+as a template to work from.
---
+++ prep.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+set -x
+
+installer_cmd=""
+package_type=""
+sudo_cmd="sudo -EH"
+
+#determin os type and installer
+type yum &> /dev/null && installer_cmd="yum install -y" package_type="rpm"
+# prefer dnf if both yum and dnf are installed e.g. fedora
+type dnf &> /dev/null && installer_cmd="dnf install -y" package_type="rpm"
+
+type apt-get &> /dev/null && installer_cmd="apt-get install -y" package_type="deb"
+
+# common packages
+packages="qemu-img debootstrap curl wget python-setuptools"
+
+# add distro specifc packages here
+if "deb" == "$package_type"; then
+    packages="$packages"
+else
+    packages="$packages"
+fi
+
+#install basic dependecies
+$sudo_cmd $installer_cmd $packages
+
+#set up diskimage builder submodule
+git submodule init
+git submodule update
+
+$sudo_cmd easy_install -U  pip
+pushd diskimage-builder
+$sudo_cmd pip install -U bindep -c https://raw.githubusercontent.com/openstack/requirements/master/upper-constraints.txt
+$sudo_cmd $installer_cmd $(bindep -b)
+$sudo_cmd pip install -U  -c https://raw.githubusercontent.com/openstack/requirements/master/upper-constraints.txt -r requirements.txt
+popd
+
+set +x
