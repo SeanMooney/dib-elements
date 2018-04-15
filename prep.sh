f477ddfd878a1a2c4ace16403cc282e90cec11e5
Sean Mooney <sean@seanmooney.info>
2018-04-15 02:40:37 +0100

    fix prep.sh for ubuntu
    - This change moves qemu-img to distro
      specific section and uses qemu-utils
      on debian based systems.

--- prep.sh
+++ prep.sh
@@ -7,20 +7,20 @@ package_type=""
 sudo_cmd="sudo -EH"
 
 #determin os type and installer
-type yum &> /dev/null && installer_cmd="yum install -y" package_type="rpm"
+type yum &> /dev/null && installer_cmd="yum install -y" package_type='rpm'
 # prefer dnf if both yum and dnf are installed e.g. fedora
-type dnf &> /dev/null && installer_cmd="dnf install -y" package_type="rpm"
+type dnf &> /dev/null && installer_cmd="dnf install -y" package_type='rpm'
 
-type apt-get &> /dev/null && installer_cmd="apt-get install -y" package_type="deb"
+type apt-get &> /dev/null && installer_cmd="apt-get install -y" package_type='deb'
 
 # common packages
-packages="qemu-img debootstrap curl wget python-setuptools"
+packages="debootstrap curl wget python-setuptools"
 
 # add distro specifc packages here
-if "deb" == "$package_type"; then
-    packages="$packages"
+if [ 'deb' == "${package_type}" ]; then
+    packages="$packages qemu-utils"
 else
-    packages="$packages"
+    packages="$packages qemu-img"
 fi
 
 #install basic dependecies
